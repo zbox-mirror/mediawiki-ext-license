f71416d504fc0a0fb07569e65978c9f02134309c
z17cx <mail@z17.cx>
2022-09-05 20:38:21 +0000

    2022-09-05 20:38:21

--- MW_EXT_License.i18n.magic.php
+++ MW_EXT_License.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @licence GNU GPL
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['en'] = [
   'license' => [0, 'license'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['ru'] = [
   'license' => [0, 'license'],
